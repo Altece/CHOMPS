3253c1eaa1fa64134f92d51134b2d0aed39dbf0e
Steven Brunwasser <steven@altece.student.rit.edu>
2013-03-09 10:42:39 -0500

    Created Xcode project for Empty Universal iOS App using CoreData and Unit Tests

---
+++ Codename CHOMPSTests/Codename_CHOMPSTests.h
@@ -0,0 +1,13 @@
+//
+//  Codename_CHOMPSTests.h
+//  Codename CHOMPSTests
+//
+//  Created by Steven Brunwasser on 3/9/13.
+//  Copyright (c) 2013 Codename CHOMPS. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Codename_CHOMPSTests : SenTestCase
+
+@end
---
+++ Codename CHOMPSTests/Codename_CHOMPSTests.m
@@ -0,0 +1,32 @@
+//
+//  Codename_CHOMPSTests.m
+//  Codename CHOMPSTests
+//
+//  Created by Steven Brunwasser on 3/9/13.
+//  Copyright (c) 2013 Codename CHOMPS. All rights reserved.
+//
+
+#import "Codename_CHOMPSTests.h"
+
+@implementation Codename_CHOMPSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Codename CHOMPSTests");
+}
+
+@end
