7a25ffabd2fceef7b30ec2efae0aee3f917a5af2
Steven Brunwasser <sbrunwasser@gmail.com>
2013-03-14 23:30:31 -0400

    You can now save, get, and delete  UIImages from Image objects in the database.

--- Codename CHOMPS/Image.h
+++ Codename CHOMPS/Image.h
@@ -9,10 +9,17 @@
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+typedef enum : NSUInteger {
+    SavedImageQuialityLow,
+    SavedImageQuialityMedium,
+    SavedImageQuialityHigh
+} SavedImageQuiality;
+
 
 @interface Image : NSManagedObject
 
-@property (nonatomic, retain) id image;
+@property (nonatomic, retain) NSString * imagePath;
 @property (nonatomic, retain) NSDate * timestamp;
+@property (nonatomic, weak) UIImage *image;
 
 @end
--- Codename CHOMPS/Image.m
+++ Codename CHOMPS/Image.m
@@ -8,10 +8,113 @@
 
 #import "Image.h"
 
+static SavedImageQuiality imageQuality = SavedImageQuialityHigh;
+
+static NSString *IMAGES_FOLDER = @"Images";
+
+static NSUInteger fileNumber = 0;
+
+static NSString *GrabImagesFolder()
+{
+    return [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:IMAGES_FOLDER];
+}
+
+/// A function to get the compression value for an
+/// ImageTransformerQuality value
+/// @param quality  The quality value.
+/// @return The comression value for the ImageTransformerQuality
+static CGFloat compressionForImageQuality(SavedImageQuiality quality)
+{
+    switch (quality) {
+        case SavedImageQuialityHigh:
+            return 1.0;
+            
+        case SavedImageQuialityMedium:
+            return 0.5;
+            
+        case SavedImageQuialityLow:
+            return 0.0;
+            
+        default:
+            return 1.0;
+    }
+}
+
+@interface Image ()
+
+- (void)deleteImage;
+
+@end
+
 
 @implementation Image
 
 @dynamic image;
+@dynamic imagePath;
 @dynamic timestamp;
+
+#pragma mark - get/set compression quality
+
++ (void)setImageQuality:(SavedImageQuiality)quality
+{
+    imageQuality = quality;
+}
+
++ (SavedImageQuiality)imageQuality
+{
+    return imageQuality;
+}
+
+#pragma mark - transforming the data to and from the image
+
+- (UIImage *)image
+{
+    if ([self.imagePath isEqualToString:@""]) {
+        NSString *filename = self.imagePath;
+        NSString *filePath = [GrabImagesFolder() stringByAppendingPathComponent:filename];
+        
+        NSData *data = [NSData dataWithContentsOfFile:filePath];
+        
+        return [UIImage imageWithData:data];
+    }
+    
+    return nil;
+}
+
+- (void)setImage:(UIImage *)image
+{
+    [self deleteImage];
+    
+    if (image) {
+        NSString *filename = [[@"Image " stringByAppendingFormat:@"%@ %d", [[NSDate date] description], fileNumber++] stringByAppendingPathExtension:@".jpg"];
+        NSString *filePath = [GrabImagesFolder() stringByAppendingPathComponent:filename];
+        
+        NSData *data = UIImageJPEGRepresentation(image, compressionForImageQuality(imageQuality));
+        
+        [data writeToFile:filePath atomically:YES];
+        
+        self.imagePath = filename;
+    } else {
+        self.imagePath = @"";
+    }
+}
+
+#pragma mark - Deleting Things
+
+- (void)deleteImage
+{
+    NSString *filename = self.imagePath;
+    if (![filename isEqualToString:@""]) {
+        NSString *filePath = [GrabImagesFolder() stringByAppendingPathComponent:filename];
+        [[NSFileManager defaultManager] removeItemAtPath:filePath error:nil];
+    }
+}
+
+- (void)prepareForDeletion
+{
+    [self deleteImage];
+    self.imagePath = @"";
+    [super prepareForDeletion];
+}
 
 @end
